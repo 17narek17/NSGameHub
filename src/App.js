678bfb7d4dc430190e05e4ad8d99b0bbc39f8acb
Sam <samvel_2002@list.ru>
2022-02-01 22:29:23 +0400

    master

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Footer />
-        <Navbar />
     </div>
   );
 }
